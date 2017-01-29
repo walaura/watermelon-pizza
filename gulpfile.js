3dfd02e21380cc214b430c73db6444900f94842a
Laura González <lawr@lauragonzalez.cc>
2017-01-29 11:50:16 +0100

    Minify css too

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
-const gulp = require('gulp');
-const webpack = require('webpack-stream');
-const uglify = require('gulp-uglify');
 const path = require('path');
 const fs = require('fs-extra');
 
+const gulp = require('gulp');
+const webpack = require('webpack-stream');
+const WebpackClearConsole = require("webpack-clear-console").WebpackClearConsole;
+const uglify = require('gulp-uglify');
+const uglifycss = require('gulp-uglifycss');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const banner = require('gulp-banner');
+
+const bannerText = fs.readFileSync('./etc/banner.txt').toString()+"\n\n";
 
 var webpackConfig = {
 	module: {
@@ -17,6 +24,7 @@ var webpackConfig = {
 			}
 		]
 	},
+	plugins: [],
 	output: {
 		filename: 'watermelon.js',
 	},
@@ -27,29 +35,56 @@ var webpackConfig = {
 
 
 gulp.task('clean',function() {
+
 	['public/watermelon','public/old'].map((dir) => fs.removeSync(dir))
 	fs.mkdirsSync('public/watermelon');
 	fs.copySync('etc/old','public/old');
+
 });
 
 
 gulp.task('makejs', function() {
+
+	webpackConfig.plugins.push(
+		new WebpackClearConsole()
+	);
+
 	return gulp.src('js/app.js')
 		.pipe(webpack(webpackConfig))
 		.pipe(uglify())
+		.pipe(banner(bannerText))
 		.pipe(gulp.dest('public/watermelon/'));
+
+});
+
+
+gulp.task('makecss',function(){
+
+	return gulp.src('./css/app.scss')
+		.pipe(sass({}).on('error', sass.logError))
+		.pipe(rename('watermelon.css'))
+		.pipe(uglifycss({}))
+		.pipe(banner(bannerText))
+		.pipe(gulp.dest('public/watermelon'));
+
 });
 
 
 gulp.task('watch',function() {
+
+	gulp.watch('./css/**/*.scss', ['makecss']);
+
 	webpackConfig.devtool = 'source-map';
 	webpackConfig.watch = true;
 	return gulp.src('js/app.js')
 		.pipe(webpack(webpackConfig))
 		.pipe(gulp.dest('public/watermelon/'));
+
 })
 
 
 gulp.task('make',['clean'],function(){
-	gulp.start('makejs');
+
+	gulp.start('makejs','makecss');
+
 })
