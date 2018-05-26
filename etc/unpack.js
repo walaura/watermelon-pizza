e96e0340553c2e58f1e21c9c4def18f3ee973241
Lau Skeeter <lawr@lauragonzalez.cc>
2018-05-26 20:39:18 +0100

    fixes

--- etc/unpack.js
+++ etc/unpack.js
@@ -3,34 +3,32 @@ const fs = require('fs');
 const rmdir = require('rmdir');
 const extract = require('extract-zip');
 
-const action = async () =>
-	new Promise(done => {
-		rmdir(path.resolve(__dirname, '..', 'public', 'old'));
-		fs.mkdirSync(path.resolve(__dirname, '..', 'public', 'old'));
+const sourcePath = path.resolve(__dirname, '..', 'etc', 'old');
+const targetPath = path.resolve(__dirname, '..', 'public', 'old');
 
-		fs.readdir(path.resolve(__dirname, 'old'), (err, files) => {
-			if (err) throw err;
-			files = files.filter(file => path.extname(file) === '.zip');
-
-			let extractedFiles = 0;
-			let onExtractionComplete = () => {
-				extractedFiles++;
-				if (extractedFiles >= files.length) done();
-			};
-
-			files.map(file => {
-				let extraction = extract(
-					'./etc/old/' + file,
-					{
-						dir: process.cwd() + '/public/old',
-					},
-					function(err) {
-						if (err) throw err;
-						onExtractionComplete();
-					}
-				);
-			});
-		});
+const extractPromise = file =>
+	new Promise((yay, nay) => {
+		extract(
+			path.resolve(sourcePath, file),
+			{
+				dir: targetPath,
+			},
+			function(err) {
+				if (err) nay(err);
+				yay(file);
+			}
+		);
 	});
 
-action().then(() => console.log('unpacked'));
+const run = async () => {
+	await new Promise(yay => rmdir(targetPath, {}, () => yay()));
+	fs.mkdirSync(targetPath);
+
+	const files = fs
+		.readdirSync(sourcePath)
+		.filter(file => path.extname(file) === '.zip');
+
+	Promise.all(files.map(extractPromise));
+};
+
+run().then(() => console.log('unpacked'));
