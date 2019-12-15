4721fd33571d282410458b1f5a878a17d25ee56d
Lau Skeeter <lawr@lauragonzalez.cc>
2019-12-15 18:20:44 +0000

    oh

--- etc/unpack.js
+++ etc/unpack.js
@@ -1,36 +1,37 @@
-const path = require('path');
-const fs = require('fs-extra');
-const extract = require('extract-zip');
-const config = require('./../.watermelonrc');
+const path = require("path");
+const fs = require("fs-extra");
+const extract = require("extract-zip");
+const config = require("./../.watermelonrc");
 
-const sourcePath = path.resolve(__dirname, '..', 'etc', 'old');
-const targetPath = path.resolve(config.files.out, 'old');
+const sourcePath = path.resolve(__dirname, "..", "etc", "old");
+const targetPath = path.resolve(config.files.out, "old");
 
 const extractPromise = file =>
-	new Promise((yay, nay) => {
-		extract(
-			path.resolve(sourcePath, file),
-			{
-				dir: targetPath,
-			},
-			function(err) {
-				if (err) nay(err);
-				yay(file);
-			}
-		);
-	});
+  new Promise((yay, nay) => {
+    console.log("unpacking " + file);
+    extract(
+      path.resolve(sourcePath, file),
+      {
+        dir: targetPath
+      },
+      function(err) {
+        if (err) nay(err);
+        yay(file);
+      }
+    );
+  });
 
 const run = async () => {
-	try {
-		fs.removeSync(targetPath);
-	} catch (e) {}
-	fs.mkdirsSync(targetPath);
+  try {
+    fs.removeSync(targetPath);
+  } catch (e) {}
+  fs.mkdirsSync(targetPath);
 
-	const files = fs
-		.readdirSync(sourcePath)
-		.filter(file => path.extname(file) === '.zip');
+  const files = fs
+    .readdirSync(sourcePath)
+    .filter(file => path.extname(file) === ".zip");
 
-	Promise.all(files.map(extractPromise));
+  Promise.all(files.map(extractPromise));
 };
 
-run().then(() => console.log('unpacked'));
+run().then(() => console.log("unpacked"));
