58ad7568e365d299a207f5922bdbd1f3ad3e7e67
Lau Skeeter <hi@laura.monster>
2020-01-01 14:16:03 +0000

    WIP

--- src/pages/old-sites.js
+++ src/pages/old-sites.js
@@ -3,10 +3,12 @@ import Layout, { WrapBlock } from "../layout/layout";
 import Stickers from "./../components/stickers/stickers";
 import { Heading } from "./../components/type/type";
 import Footer from "./../components/footer/footer";
+import { Link } from "gatsby";
 
 export default () => (
   <Layout title="Wander into the past">
     <WrapBlock inverted outside={<Stickers avoidBackground={false} />}>
+      <Link to="/">Backsies</Link>
       <Heading>Wander into the past</Heading>
     </WrapBlock>
     <WrapBlock>
