9d12a5144cdc70b6ad24c0b9fd624cf2bd6a51f2
Lau Skeeter <hi@laura.monster>
2020-03-01 13:35:07 +0000

    vscode

--- src/components/stickers/stickers/index.js
+++ src/components/stickers/stickers/index.js
@@ -1,11 +1,12 @@
 export const stickers = [
-  require("./thumbos.png"),
-  require("./skull.png"),
-  require("./star.png"),
-  require("./wyld.png"),
-  require("./ace-rat.png"),
-  require("./pronoun.png"),
-  require("./blush.png"),
-  require("./css.png"),
-  require("./luigi-bride.png")
+	require('./thumbos.png'),
+	require('./skull.png'),
+	require('./star.png'),
+	require('./wyld.png'),
+	require('./ace-rat.png'),
+	require('./pronoun.png'),
+	require('./blush.png'),
+	require('./vsco.png'),
+	require('./css.png'),
+	require('./luigi-bride.png'),
 ];
