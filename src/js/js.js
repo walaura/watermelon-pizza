1bd7d177f56e86f1328c39654e41f79a2d1b5218
walaura <hi@laura.monster>
2026-05-02 20:23:45 +0100

    fghfg

--- src/js/js.js
+++ src/js/js.js
@@ -1,2 +1,18 @@
 const $year = document.querySelector("x-year");
 $year.innerText = new Date().getFullYear();
+
+const links = document.querySelector("nav.links");
+
+links.addEventListener("wheel", (e) => {
+  if (Math.abs(e.deltaX) > Math.abs(e.deltaY)) {
+    return;
+  }
+
+  // Check if the element can scroll horizontally
+  const canScrollHorizontally = links.scrollWidth > links.clientWidth;
+  if (!canScrollHorizontally) {
+    return;
+  }
+  e.preventDefault();
+  links.scrollLeft += e.deltaY;
+});
